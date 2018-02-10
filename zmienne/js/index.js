468a114558e3617b4cae2d3f4dced384c07890de
MArcin Dębski <marcin.m.debski@gmail.com>
2018-02-10 16:59:43 +0100

    funkcje

---
+++ zmienne/js/index.js
@@ -0,0 +1,20 @@
+/*
+komicik blokowy 
+*/
+// komicik liniowy tylko do konca linijeczki
+//czbcfbfdz
+//bzfbzfdbdzbdfzb
+//scrypt w body najlepiej na samym dole w body
+var imie = "Marcin";
+var nazwisko = "Dębski";
+var wiek = "26";
+var wzrost;
+var wynik = "Imie: " + imie + " Nazwisko: " + nazwisko + " wiek:" + wiek;
+
+
+function pokazDane() {
+    
+document.write( imie, nazwisko, wiek);
+}
+
+pokazDane();
