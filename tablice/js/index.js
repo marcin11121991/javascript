bb5a109502d498b7437575454a034fbf83bb831e
MArcin Dębski <marcin.m.debski@gmail.com>
2018-02-11 10:56:18 +0100

    dodawanie elementu do tablicy

---
+++ tablice/js/index.js
@@ -0,0 +1,11 @@
+var imiona = ["paweł","Krzysztof","Kasia", "Nikol", "Kamil" ];
+
+console.log(imiona[2]);
+console.log(imiona);
+
+
+imiona[5] = "Monika";
+imiona[6] = "marcin";
+
+console.log(imiona[5]);
+console.log(imiona);
