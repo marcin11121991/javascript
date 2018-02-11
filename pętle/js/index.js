c8d6a10547584c904b1252f046d17e6aeedb8d59
MArcin Dębski <marcin.m.debski@gmail.com>
2018-02-11 16:30:20 +0100

    petla while i do while

--- pętle/js/index.js
+++ pętle/js/index.js
@@ -16,3 +16,25 @@ for (var i = 0; i < tablica.length; i++){
 tablica.forEach( function(elementTablicy, indexTablicy){
     console.log("Index: " + indexTablicy + ", wartość: " + elementTablicy);
 })
+
+// petle while i do while
+
+
+var iter = 0;
+
+
+while( iter < 20 ) {
+    console.log( iter );
+    ++iter;
+}
+
+
+
+var iterek = 30;
+do {
+    console.log(iterek);
+    ++iterek;
+} while ( iterek < 10);
+
+
+
