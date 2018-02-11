c3740861ef85e494143c74938015cb47f8d5bdcb
MArcin Dębski <marcin.m.debski@gmail.com>
2018-02-11 12:55:15 +0100

    klasy w js pierwsza fajna rzecz

--- obiekty/js/index.js
+++ obiekty/js/index.js
@@ -15,4 +15,37 @@ var osoba = {
 osoba.wyswietlSzczegoly();
 osoba.wyswietlSzczegoly("igoreczek");
 
-console.log(osoba.wyswietlSzczegoly);
+//console.log(osoba.wyswietlSzczegoly);
+
+
+// metoda dla jednego obiektu wyżej 
+
+// dodawwanie wlasnosci do obiektu
+
+osoba.wiek = 36;
+osoba.wyswietlWiek = function() {
+    console.log(this.wiek);
+    
+}
+
+osoba.wyswietlWiek();
+/// matryca klasa
+
+
+class Ogloszenie{
+    constructor(tytul, opis, kontakt, cena){
+        this.tytul = tytul;
+        this.opis = opis;
+        this.kontakt = kontakt;
+        this.cena = cena;
+    }
+    wyswietlOgloszenie() {
+        document.write("Tytuł: " + this.tytul + " " + "opis: " + this.opis + " " + "kontakt" + this.kontakt + " " + "cena: " + this.cena );
+    }
+} 
+
+var ogloszenie1 = new Ogloszenie("Sprzedam Opla", "Niemiec płakał jak sprzedawał", "666-656-656", 1600);
+ogloszenie1.wyswietlOgloszenie();
+
+var ogloszenie2 = new Ogloszenie("Sprzedam passerat", "Niemiec gonił do granicy", "0770-700-700", 6666);
+ogloszenie2.wyswietlOgloszenie();
