58e3edb5502d3782943b58ff57dec55a522bb9a0
MArcin Dębski <marcin.m.debski@gmail.com>
2018-02-11 11:41:49 +0100

    tablicunie

--- tablice/js/index.js
+++ tablice/js/index.js
@@ -1,4 +1,4 @@
-var imiona = ["paweł","Krzysztof","Kasia", "Nikol", "Kamil" ];
+var imiona = ["paweł", "Krzysztof", "Kasia", "Nikol", "Kamil"];
 
 console.log(imiona[2]);
 console.log(imiona);
@@ -9,3 +9,56 @@ imiona[6] = "marcin";
 
 console.log(imiona[5]);
 console.log(imiona);
+
+imiona.push("karolina");
+console.log(imiona);
+
+
+
+imiona.pop();
+console.log(imiona); /// usuwa ostani element tablicy
+
+
+var usunietyElement = imiona.pop();
+console.log(usunietyElement);
+
+
+
+// imiona.shift();
+// console.log(imiona); usuwanie pierwszego elelemntu 
+
+
+///// długosć tablicy
+
+console.log(imiona.length);
+
+
+// rozbijanie tablicy na ciąg tekstowy
+
+
+var tablicaJakoTekst = imiona.join(" ");
+
+console.log(tablicaJakoTekst);
+
+//// odwracanie tablicy
+
+var odwroconaTablica = imiona.reverse();
+console.log(odwroconaTablica);
+
+var sortTablica = imiona.sort();
+console.log(sortTablica);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
