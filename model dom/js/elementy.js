3484d6b17beca982abb5ade5caa1fb4c54152250
MArcin Dębski <marcin.m.debski@gmail.com>
2018-02-17 12:01:19 +0100

    dom poczatek

---
+++ model dom/js/elementy.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var sectionFirst = document.getElementById("parFirst");
+console.log(sectionFirst);
+
+var link = document.getElementsByClassName("superlink");
+console.log(link);
+
+var linkiPoTagu = document.getElementsByTagName('a');
+console.log(linkiPoTagu);
+
+var divPoID = document.querySelector('#parSecond');
+console.log(divPoID);
+
+var linkiPoSelektorze = document.querySelectorAll('.superlink');
+console.log(linkiPoSelektorze);
---
+++ model dom/js/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log(window);
+console.log(window.innerHeight);
+console.log(window.outerHeight);
+
+
+
+console.log(document);
