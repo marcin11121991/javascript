76e48aa42c415c52c839baa3425c6b7c12f16f48
MArcin Dębski <marcin.m.debski@gmail.com>
2018-02-17 14:03:20 +0100

    dodawania i zmiany elelemntow

--- relacje/js/operacjenawezlach.js
+++ relacje/js/operacjenawezlach.js
@@ -14,3 +14,16 @@ istniejacyWezel.removeChild(newElement);
 
 var parFirstDiv = document.getElementById('parFirst');
 parFirstDiv.replaceChild(newElement, istniejacyWezel);  // zmiana elementu na wezle
+
+var allLinks = document.querySelectorAll('a'); /// wybieramy wszystkie a
+// console.log(allLinks);
+for (var i = 0; i < allLinks.length; i++) {
+    var br = document.createElement('br');
+    allLinks[i].parentNode.insertBefore(br, allLinks[i].nextSibling); 
+    allLinks[i].removeAttribute('class');
+}
+// dodaj br po kazdym linku
+
+
+
+
